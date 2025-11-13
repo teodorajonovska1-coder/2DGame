5135f0fe8d477a2fb1681cfc962f83240e64a381
teodorajonovska1-coder <teodora.jonovska1@gmail.com>
2025-11-13 14:26:09 +0100

    5cas
    14 video done

---
+++ Assets/Scripts/LoadingManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingManager : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+            SceneManager.LoadScene(1);
+    }
+}
